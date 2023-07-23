4da63bff82f14b2f698071aa5b6282904e557c99
Severin Bruhat <sbruhat@gmail.com>
2023-07-23 18:04:28 +0100

    use the v1 API to post the image

--- main.py
+++ main.py
@@ -21,11 +21,20 @@ cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-# Authenticate with Twitter API
+# Authenticate with Twitter API v2
 client = tweepy.Client(consumer_key=twitter_consumer_key,
                        consumer_secret=twitter_consumer_secret,
                        access_token=twitter_access_token,
                        access_token_secret=twitter_access_token_secret)
+
+# Authenticate with Twitter API v1 (v2 doesn't expose a media endpoint yet)
+auth = tweepy.OAuth1UserHandler(
+    twitter_consumer_key,
+    twitter_consumer_secret,
+    twitter_access_token,
+    twitter_access_token_secret
+)
+api = tweepy.API(auth)
 
 # array of hashtags
 hashtags = [
@@ -89,29 +98,43 @@ def generate_image(text):
     return False
 
 
+def upload_image_to_twitter(image_path, alt_text):
+    media = api.media_upload(filename=image_path)
+    media_id = media.media_id_string
+    api.create_media_metadata(media_id=media_id, alt_text=alt_text)
+
+    return media_id
+
 # Function to post a comment on Twitter and update the flag in Firestore
+
 
 def post_comment(comment):
     tweet = comment["content"]["text"]
     tweet_id = comment["id"]
-    try:
-        random_hashtag = random.choice(hashtags)
-        client.create_tweet(text=tweet + " " + random_hashtag)
-        print("Comment posted on Twitter:", tweet)
-    except tweepy.TweepError as e:
-        print("Error occurred while posting the comment:", e)
-        return False
+    image_path = generate_image(tweet)
 
-    try:
-        # Update the flag to true in Firestore
-        comment_ref = db.collection("comments").document(tweet_id)
-        comment_ref.update({"flag": True})
-        print("Flag updated to True in Firestore.")
-    except Exception as e:
-        print("Error occurred while updating the flag in Firestore:", e)
-        return False
+    if image_path != False:
+        try:
+            media_id = upload_image_to_twitter(image_path, tweet)
+            random_hashtag = random.choice(hashtags)
+            client.create_tweet(text=tweet + " " +
+                                random_hashtag, media_ids=[media_id])
+            # print("Comment posted on Twitter:", tweet)
+        except tweepy.TweepError as e:
+            print("Error occurred while posting the comment:", e)
+            return False
 
-    return True
+        try:
+            # Update the flag to true in Firestore
+            comment_ref = db.collection("comments").document(tweet_id)
+            comment_ref.update({"flag": True})
+            # print("Flag updated to True in Firestore.")
+        except Exception as e:
+            print("Error occurred while updating the flag in Firestore:", e)
+            return False
+        return True
+
+    return False
 
 
 # Get a random comment and post it on Twitter
@@ -120,4 +143,4 @@ if random_comment:
     if not post_comment(random_comment):
         print("Failed to post the comment and update the flag.")
     else:
-        print("No comments with null or false flag found in Firestore.")
+        print("All good!")
